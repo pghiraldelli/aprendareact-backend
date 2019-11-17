95ddfb62f72c7b66461cd64143735addd0b63696
Patricia <psantosg@thoughtworks.com>
2019-11-17 16:29:19 -0300

    :chore: Adds recommended config for eslint and removes unused dependencies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('@babel/register')
-const server = require("./src/core/serverConfig").default
+const server = require('./src/core/serverConfig').default
 
 server.listen(8080, () => console.log('App running in port 8080'))
--- src/core/serverConfig.js
+++ src/core/serverConfig.js
@@ -1,5 +1,5 @@
-import express from 'express';
+import express from 'express'
 
-const server = express();
+const server = express()
 
-export default server;
+export default server
