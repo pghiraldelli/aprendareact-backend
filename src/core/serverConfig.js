77d52c473fcdf8471eef18de0ef34bdc20a0d0d3
Patricia <psantosg@thoughtworks.com>
2019-11-18 20:34:05 -0300

    :chore: Adds express middlewares

--- src/core/serverConfig.js
+++ src/core/serverConfig.js
@@ -1,5 +1,32 @@
 import express from 'express'
+import cookieSession from 'cookie-session'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
-const server = express()
+const oneDayInMiliseconds = 24 * 60 * 60 * 100
+const cookieKey = 'holland seems cool what about winter'
 
-export default server
+const app = express()
+
+app.use(
+    cookieSession({
+        name: 'session',
+        keys: ['secret'],
+        maxAge: oneDayInMiliseconds
+    })
+)
+
+app.use(bodyParser.json())
+
+app.use(cookieParser(cookieKey))
+
+app.use(
+    cors({
+        origin: process.env.HOST,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true
+    })
+)
+
+export default app
