3b8575b4f60bef2a3799747f24dfa3d1a5d56245
Patricia <psantosg@thoughtworks.com>
2019-11-22 18:50:33 -0300

    :test: Fix error in integration test

--- src/auth/google/authGoogle.js
+++ src/auth/google/authGoogle.js
@@ -2,8 +2,8 @@ import { Strategy } from 'passport-google-oauth20'
 
 const googleStrategy = new Strategy(
   {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID || 'dummy-client-id',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'dummy-client-secret',
     callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, cb) => { 
