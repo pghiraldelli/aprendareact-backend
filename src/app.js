59d9604fbf3c4fa59d606800cf3b3534c56311b3
Patricia <psantosg@thoughtworks.com>
2019-11-27 14:32:19 -0300

    :feat: Adds payment endpoint

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import { server as app, passport } from './config/server'
 import healthCheck from './controllers/healthCheck'
+import payment from './controllers/payment'
 
 app.get('/health', healthCheck)
 
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }))
 
-app.get( '/auth/google/callback',
+app.get('/auth/google/callback',
   passport.authenticate( 'google', {
     successRedirect: `${process.env.HOST}/cursos/iniciando-em-react`,
     failureRedirect: `${process.env.HOST}/login/failed`
   })
 )
+
+app.post('/payment', payment)
 
 export default app
