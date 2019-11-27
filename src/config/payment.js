025073be710cf22b4e54eb947349812d87089711
Patricia <psantosg@thoughtworks.com>
2019-11-26 21:38:10 -0300

    :chore: Adds mercadopago dependency and its configuration

---
+++ src/config/payment.js
@@ -0,0 +1,8 @@
+import mercadopago from 'mercadopago'
+
+mercadopago.configure({
+  sandbox: process.env.NODE_ENV == 'test',
+  access_token: process.env.MERCADO_PAGO_TOKEN
+})
+
+export default mercadopago
